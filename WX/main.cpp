b20fd4c6afd4615b098c35d36fb528ce21b46521
wwwwenxin <578355367@qq.com>
2015-11-25 19:22:40 +0800

    向用户提问是否下雨
    向用户提问“Is it rainning now”，提示用户若输入“Y”,显示“It is rainning now
    ”；若输入”N“，则显示“It is not rainning now ”。

--- WX/main.cpp
+++ WX/main.cpp
@@ -7,9 +7,28 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    bool  b=true;
+    while( b )
+    {
+        cout<<"Is it rainning now？请输入'Y'或者‘N’";
+        char c;
+        cin>>c;
+        switch(c)
+        {
+            case 'Y':
+                cout<<"It is rainning now "<<endl;
+                b=false;
+                break;
+            case 'N':
+                cout<<"It is not rainning now"<<endl;
+                b=false;
+                break;
+            default:
+                cout<<"It is wrong that you input!"<<endl;
+        }
+    }
     return 0;
 }
